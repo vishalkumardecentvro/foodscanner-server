22fffeb5aa4bb7fba9b800004b9a7efa6e7ad1b1
vishalkumardecentvro <vishalvlc01@gmail.com>
2022-11-12 01:03:31 +0530

    nutrient detail api added

--- app.js
+++ app.js
@@ -38,8 +38,30 @@ app.get('/product/:barcode',(req,res)=>{
     })
 })
 
-app.get('/product/nutrients/:product_id',(req,res)=>{
-    let sql = `select * from nutrients where product_id = ${req.params.product_id}`
+app.get('/product/ingredient/:product_id',(req,res)=>{
+    let sql = `select * from ingredient where product_id = ${req.params.product_id}`
+    db.query(sql,(err,result)=>{
+        if(err){
+            res.send("Something wrong with api buddy")
+        }
+        console.log(result)
+        res.send(result)
+    })
+})
+
+app.get('/product/nutrient/:product_id',(req,res)=>{
+    let sql = `select * from nutrient where product_id = ${req.params.product_id}`
+    db.query(sql,(err,result)=>{
+        if(err){
+            res.send("Something wrong with api buddy")
+        }
+        console.log(result)
+        res.send(result)
+    })
+})
+
+app.get('/product/nutrient/details/:nutrient_id',(req,res)=>{
+    let sql = `select details from all_nutrients where id = ${req.params.nutrient_id}`
     db.query(sql,(err,result)=>{
         if(err){
             res.send("Something wrong with api buddy")
